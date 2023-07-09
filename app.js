858add3ac65c0c473d0fc7e3e5b7af54f896d8bb
Jswears <ji.swearssalinas@gmail.com>
2023-07-09 21:41:15 +0200

    done

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 require("dotenv").config();
-
-const { configDotenv } = require("dotenv");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
-// require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
+// Set up middleware
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-// setting the spotify-api goes here:
+// Configure Spotify API
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
@@ -23,96 +21,92 @@ const spotifyApi = new SpotifyWebApi({
 // Retrieve an access token
 const getToken = async () => {
   try {
-    let data = await spotifyApi.clientCredentialsGrant();
+    const data = await spotifyApi.clientCredentialsGrant();
     spotifyApi.setAccessToken(data.body["access_token"]);
-    // console.log(data.body.access_token);
   } catch (error) {
-    console.log("There was an error ", error);
+    console.log("There was an error:", error);
   }
 };
 
 // Get artist
 const getArtist = async (artist) => {
   try {
-    let data = await spotifyApi.searchArtists(artist);
-    // console.log(data);
+    const data = await spotifyApi.searchArtists(artist);
     return data;
   } catch (error) {
-    console.log("There has been an error ", error);
+    console.log("There has been an error:", error);
   }
 };
 
-//Caller function
+// Initialize the app
 const initApp = async () => {
   await getToken();
-  await getArtist();
 };
 
-//Call caller function
+// Call initialization function
 initApp();
 
-// Our routes go here:
-app.get("/", (request, response) => {
+// Define routes
+app.get("/", (req, res) => {
   const pageTitle = "Espotifai";
   const pageStyling = "/styles/index.css";
-  response.render("index", { pageTitle, pageStyling });
+  res.render("index", { pageTitle, pageStyling });
 });
 
-app.get("/artist-search", async (request, response) => {
-  //artist
-  const artistSearch = request.query["artist-search"];
+app.get("/artist-search", async (req, res) => {
+  const artistSearch = req.query["artist-search"];
 
   if (!artistSearch) {
-    response.send("No search provided");
+    res.send("No search provided");
     return;
   }
+
   try {
-    // console.log("Input value", artistSearch);
     const data = await getArtist(artistSearch);
     const artist = data.body.artists.items;
     const pageTitle = artist[0].name;
     const pageStyling = "/styles/artist-style.css";
-    response.render("artist-search-results", {
+    res.render("artist-search-results", {
       artist: artist[0],
       pageTitle,
       pageStyling,
     });
   } catch (error) {
-    console.log("There has been an error", error);
-    response.render("error");
+    console.log("There has been an error:", error);
+    res.render("error");
   }
 });
 
-app.get("/all-albums/:artistId", async (request, response) => {
-  const artistId = request.params.artistId;
+app.get("/all-albums/:artistId", async (req, res) => {
+  const artistId = req.params.artistId;
   const pageTitle = "Albums";
   const pageStyling = "/styles/album-style.css";
 
   try {
     const data = await spotifyApi.getArtistAlbums(artistId);
     const albums = data.body.items;
-    response.render("all-albums", { albums, pageTitle, pageStyling });
+    res.render("all-albums", { albums, pageTitle, pageStyling });
   } catch (error) {
-    console.log("There has been an error ", error);
+    console.log("There has been an error:", error);
   }
 });
 
-app.get("/:name/:albumId", async (request, response) => {
-  const name = request.params.name;
-  const albumId = request.params.albumId;
+app.get("/tracks/:albumId", async (req, res) => {
+  const albumId = req.params.albumId;
   const pageStyling = "/styles/tracks.css";
 
   try {
     const data = await spotifyApi.getAlbumTracks(albumId);
     const tracks = data.body.items;
     const pageTitle = "Songs";
-    console.log(tracks);
-    response.render("track-information", { tracks, pageTitle, pageStyling });
+    res.render("track-information", { tracks, pageTitle, pageStyling });
   } catch (error) {
-    console.log("There has been an error ", error);
+    console.log("There has been an error:", error);
   }
 });
 
-app.listen(3000, () =>
-  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+// Start the server
+const port = 3000;
+app.listen(port, () =>
+  console.log(`My Spotify project running on port ${port} 🎧 🥁 🎸 🔊`)
 );
